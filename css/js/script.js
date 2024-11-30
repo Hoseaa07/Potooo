0b77566134a48a27c0a0ca19b73e609114bad387
Hoseaa07 <hoseabarnabylie@gmail.com>
2024-11-30 17:44:46 +0700

    bug fixed v1.1.0

--- css/js/script.js
+++ css/js/script.js
@@ -3,12 +3,13 @@ window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
 
-    if (document.documentElement.scrollTop > fixedNav) {
-        header.classList.add('navbar-fixed')
+    if (window.scrollY > fixedNav) {
+        header.classList.add('navbar-fixed');
     } else {
-        header.classList.remove('navbar-fixed')
+        header.classList.remove('navbar-fixed');
     }
-}
+};
+
 
 // Hamburger Menu
 const hamburger = document.querySelector('#hamburger');
